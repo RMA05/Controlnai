1820e923a2d387d4f4ba42d7b711bb338b4b1945
Murad <11_rma@mail.ru>
2022-05-11 21:02:56 +0300

    Готовая задача

--- Program.cs
+++ Program.cs
@@ -9,3 +9,26 @@ for (int index = 0; index < array.Length; index++)
         count++;
     }
 }
+//Создать новый массив равное размером колличеством этих элементов.
+string[] Array = new string[count];
+int indexx = 0;
+for (int i = 0; i < array.Length; i++)
+{
+//Записываем элементы который мы вывели в новый масиив.
+    if (array[i].Length < 4)
+    {
+        Array[indexx] = array[i];
+        indexx++;
+    }
+}
+//Выводим изночальный заданый массив и конечный на печать.
+PrintArray(array);
+PrintArray(Array);
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + "  |  ");
+    }
+    System.Console.WriteLine();
+}
