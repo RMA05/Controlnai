77a9a8ce5da3437775ad3bc2ff58efa03ff35565
Murad <11_rma@mail.ru>
2022-05-11 20:54:08 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Из заданного массива,находим элемнты массива котрый меньше или равно 3-м символам - это главный алгоритм.
+string[] array = { "Hello", "2", "mouse", "ok", "-2" };
+
+int count = 0;
+for (int index = 0; index < array.Length; index++)
+{
+    if (array[index].Length < 4)
+    {
+        count++;
+    }
+}
